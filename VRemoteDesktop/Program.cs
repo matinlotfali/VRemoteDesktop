854701396d6aa4409776fbc322e58baa6041bba7
Matin Lotfaliee <matinlotfali@gmail.com>
2016-05-28 22:17:53 +0430

    Simple Read

---
+++ VRemoteDesktop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRemoteDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient(7711, AddressFamily.InterNetwork);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 7711);
+            for (;;)
+            {
+                byte[] bytes = client.Receive(ref ip);
+                string str = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
